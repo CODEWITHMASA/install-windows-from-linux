83b2d5892c6cfe73cc1b2c4cd41ae3ae4a49ad00
CODE WITH MASA <x7x1xtechnology@gmail.com>
2025-10-02 08:04:04 +0300

    Update them.sh

--- them.sh
+++ them.sh
@@ -19,17 +19,31 @@ if [ ! -d "/usr/share/icons/Windows-10-Icons" ]; then
     sudo git clone https://github.com/B00merang-Artwork/Windows-10-Icons.git /usr/share/icons/Windows-10-Icons
 fi
 
-# ضبط الثيم والأيقونات كإعداد افتراضي
+# إعدادات GTK (للقوائم والنوافذ)
 mkdir -p ~/.config/gtk-3.0
 cat <<EOF > ~/.config/gtk-3.0/settings.ini
 [Settings]
 gtk-theme-name=Windows-10
 gtk-icon-theme-name=Windows-10-Icons
+gtk-font-name=Sans 10
 EOF
 
-# ضبط LXDE يطبّق الثيم أوتوماتيك عند تسجيل الدخول
-mkdir -p ~/.config/lxsession/LXDE
-echo '@lxappearance --command=apply' >> ~/.config/lxsession/LXDE/autostart
+# إعدادات openbox (مدير النوافذ في LXDE)
+mkdir -p ~/.config/openbox
+cat <<EOF > ~/.config/openbox/lxde-rc.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<openbox_config xmlns="http://openbox.org/3.4/rc" xmlns:xi="http://www.w3.org/2001/XInclude">
+  <theme>
+    <name>Windows-10</name>
+    <titleLayout>NLIMC</titleLayout>
+    <keepBorder>yes</keepBorder>
+    <animateIconify>yes</animateIconify>
+  </theme>
+</openbox_config>
+EOF
+
+# إعادة تحميل إعدادات openbox
+openbox --reconfigure
 
 # تحميل الخلفية اللي انت عايزها
 wget -O ~/wallpaper.jpg "https://c.top4top.io/p_3560n0o481.jpg"
@@ -38,6 +52,6 @@ wget -O ~/wallpaper.jpg "https://c.top4top.io/p_3560n0o481.jpg"
 pcmanfm --set-wallpaper="$HOME/wallpaper.jpg"
 
 echo "=============================================="
-echo " ✔ تم التثبيت! "
-echo " ✨ أعد تشغيل سطح المكتب أو الـ VNC لرؤية ثيم ويندوز + الخلفية"
+echo " ✔ تم التثبيت والتطبيق!"
+echo " ✨ لو ما ظهرش التغيير، اعمل Logout/Login أو اكتب: openbox --restart"
 echo "=============================================="
