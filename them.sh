31a0fc8ad6e6040fb2620b4a9226968e820a6273
CODE WITH MASA <x7x1xtechnology@gmail.com>
2025-10-02 08:06:51 +0300

    Update them.sh

--- them.sh
+++ them.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 
-echo "=============================================="
-echo "  تثبيت وتطبيق ثيم Windows 10 على LXDE"
-echo "=============================================="
+# تحميل الثيم + الأيقونات
+apt-get update
+apt-get install -y wget unzip
 
-# تحديث الباكجات
-sudo apt-get update
+cd /tmp
+wget -O win10-theme.zip https://github.com/B00merang-Project/Windows-10/archive/refs/heads/master.zip
+unzip win10-theme.zip
+wget -O win10-icons.zip https://github.com/B00merang-Project/Windows-10-Icons/archive/refs/heads/master.zip
+unzip win10-icons.zip
 
-# تثبيت الأدوات المطلوبة
-sudo apt-get install -y git lxappearance wget
+# نقلهم للمكان الصح
+mkdir -p /usr/share/themes /usr/share/icons
+cp -r Windows-10-master /usr/share/themes/Windows-10
+cp -r Windows-10-Icons-master /usr/share/icons/Windows-10-Icons
 
-# تحميل الثيمات والأيقونات (لو مش متثبتة قبل كده)
-if [ ! -d "/usr/share/themes/Windows-10" ]; then
-    sudo git clone https://github.com/B00merang-Project/Windows-10.git /usr/share/themes/Windows-10
-fi
-
-if [ ! -d "/usr/share/icons/Windows-10-Icons" ]; then
-    sudo git clone https://github.com/B00merang-Artwork/Windows-10-Icons.git /usr/share/icons/Windows-10-Icons
-fi
-
-# إعدادات GTK (للقوائم والنوافذ)
-mkdir -p ~/.config/gtk-3.0
-cat <<EOF > ~/.config/gtk-3.0/settings.ini
+# إعدادات GTK
+mkdir -p /root/.config/gtk-3.0
+cat <<EOF > /root/.config/gtk-3.0/settings.ini
 [Settings]
 gtk-theme-name=Windows-10
 gtk-icon-theme-name=Windows-10-Icons
 gtk-font-name=Sans 10
 EOF
 
-# إعدادات openbox (مدير النوافذ في LXDE)
-mkdir -p ~/.config/openbox
-cat <<EOF > ~/.config/openbox/lxde-rc.xml
+# إعدادات Openbox
+mkdir -p /root/.config/openbox
+cat <<EOF > /root/.config/openbox/lxde-rc.xml
 <?xml version="1.0" encoding="UTF-8"?>
-<openbox_config xmlns="http://openbox.org/3.4/rc" xmlns:xi="http://www.w3.org/2001/XInclude">
+<openbox_config xmlns="http://openbox.org/3.4/rc">
   <theme>
     <name>Windows-10</name>
     <titleLayout>NLIMC</titleLayout>
@@ -42,16 +38,7 @@ cat <<EOF > ~/.config/openbox/lxde-rc.xml
 </openbox_config>
 EOF
 
-# إعادة تحميل إعدادات openbox
+# إعادة تحميل Openbox
 openbox --reconfigure
 
-# تحميل الخلفية اللي انت عايزها
-wget -O ~/wallpaper.jpg "https://c.top4top.io/p_3560n0o481.jpg"
-
-# ضبط الخلفية كافتراضية
-pcmanfm --set-wallpaper="$HOME/wallpaper.jpg"
-
-echo "=============================================="
-echo " ✔ تم التثبيت والتطبيق!"
-echo " ✨ لو ما ظهرش التغيير، اعمل Logout/Login أو اكتب: openbox --restart"
-echo "=============================================="
+echo "✅ تم تثبيت ثيم Windows 10 وتفعيله!"
